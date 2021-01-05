82980e5d9248316095e80cf9b7f8e7d5f130554c
knee-rel <nirelmarieibarra@gmail.com>
2021-01-05 19:55:48 +0800

    first commit

---
+++ u-trade/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+// require("dotenv").config({path: 'ENV_FILENAME'});
+const dotenv = require("dotenv");
+dotenv.config();
+//import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+
+const app = express();
+
+// mongoose.Promise = global.Promise
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  throw "failed to connect to MongoDB";
+});
+
+//middlewear
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+//routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
